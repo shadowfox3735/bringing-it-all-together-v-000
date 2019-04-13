237a3316edcecc8bd71ec5c5042141d0c30233cb
shadowfox3735 <normanpan180@gmail.com>
2019-04-13 02:21:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,15 @@ class Dog
   end
   
   def self.find_by_breed(breed)
-    
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def update
