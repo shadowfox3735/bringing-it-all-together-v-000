a11c7873f701bbfd3f046b6906acf1e9dbd79834
shadowfox3735 <normanpan180@gmail.com>
2019-04-13 00:42:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,17 @@ class Dog
   end
   
   def save
-    
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
 end
