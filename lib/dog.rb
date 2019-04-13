c14fbae440a4f24f6c5b23ea045ec7d701b93967
shadowfox3735 <normanpan180@gmail.com>
2019-04-13 02:21:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,6 +49,10 @@ class Dog
     end.first
   end
   
+  def self.find_by_breed(breed)
+    
+  end
+  
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
